84b6951644321394b885b81f1f6c6ebca9020b85
Tmier <itmier@126.com>
2023-12-04 23:13:03 +0800

    fix: eslint问题修复

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,11 @@
+/*
+ * @Author: Tmier
+ * @Date: 2023-08-30 22:48:30
+ * @LastEditTime: 2023-12-04 22:07:24
+ * @LastEditors: Tmier
+ * @Description:
+ *
+ */
 module.exports = {
     "env": {
         "browser": true,
@@ -31,5 +39,6 @@ module.exports = {
         "react"
     ],
     "rules": {
+      "react/react-in-jsx-scope": "off"
     }
 }
